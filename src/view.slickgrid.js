d50c58fe2530d2b9b14191c123921b14229c9112
aliounedia <dia.aliounes@gmail.com>
2014-03-27 23:16:38 +0100

    update

--- src/view.slickgrid.js
+++ src/view.slickgrid.js
@@ -420,6 +420,35 @@ my.SlickGrid = Backbone.View.extend({
       dd.helper.remove();
       $(dd.available).css("background", "beige");
     });
+    
+    $.drop({mode: "mouse"});
+    $("#dropzone")
+      .bind("dropstart", function (e, dd) {
+        if (dd.mode != "recycle") {
+          return;
+        }
+        $(this).css("background", "yellow");
+      })
+      .bind("dropend", function (e, dd) {
+        if (dd.mode != "recycle") {
+          return;
+        }
+        $(dd.available).css("background", "pink");
+      })
+      .bind("drop", function (e, dd) {
+        if (dd.mode != "recycle") {
+          return;
+        }
+        var rowsToDelete = dd.rows.sort().reverse();
+        for (var i = 0; i < rowsToDelete.length; i++) {
+          data.splice(rowsToDelete[i], 1);
+        }
+        grid.invalidate();
+        grid.setSelectedRows([]);
+      });
+
+
+
     /* end row reordering support*/
     self.grid.registerPlugin(moveRowsPlugin);
     /*
