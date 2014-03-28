ed9abe25f8712f43d2ab94f9ffcad3fb26b89720
aliounedia <dia.aliounes@gmail.com>
2014-03-28 19:08:03 +0100

    update

--- src/view.slickgrid.js
+++ src/view.slickgrid.js
@@ -300,7 +300,6 @@ my.SlickGrid = Backbone.View.extend({
       cancelEditOnDrag: true
     });
 
-    /*
     moveRowsPlugin.onBeforeMoveRows.subscribe(function (e, data) {
       for (var i = 0; i < data.rows.length; i++) {
         // no point in moving before or after itself
@@ -311,7 +310,6 @@ my.SlickGrid = Backbone.View.extend({
       }
       return true;
     });
-    */
     
     moveRowsPlugin.onMoveRows.subscribe(function (e, args) {
       
@@ -347,126 +345,14 @@ my.SlickGrid = Backbone.View.extend({
       data = left.concat(extractedRows.concat(right));
       var selectedRows = [];
       for (var i = 0; i < rows.length; i++)
-        selectedRows.push(left.length + i);
-
-      //self.grid.resetActiveCell();
-      //self.grid.setData(data);
-      //self.grid.setSelectedRows(selectedRows);
-      //self.grid.render();
-      
+        selectedRows.push(left.length + i);      
       self.model.records.reset(data)
       
     });
-    
-    /*
-    //Drag init Handler
-    this.grid.onDragInit.subscribe(function (e, dd) {
-      // prevent the grid from cancelling drag'n'drop by default
-      e.stopImmediatePropagation();
-    });
-    */
-    //Drag start Handler
-
-    /*this.grid.onDragStart.subscribe(function (e, dd) {
-      var cell = self.grid.getCellFromEvent(e);
- 
-      if (!cell) {
-        return;
-      }
-
-      dd.row = cell.row;
-      if (!data[dd.row]) {
-        return;
-      }
-
-      if (Slick.GlobalEditorLock.isActive()) {
-        return;
-      }
-
-      e.stopImmediatePropagation();
-      dd.mode = "recycle";
-
-      var selectedRows = self.grid.getSelectedRows();
-
-      if (!selectedRows.length || $.inArray(dd.row, selectedRows) == -1) {
-        selectedRows = [dd.row];
-        self.grid.setSelectedRows(selectedRows);
-      }
-
-      dd.rows = selectedRows;
-      dd.count = selectedRows.length;
-
-      var proxy = $("<span></span>")
-          .css({
-            position: "absolute",
-            display: "inline-block",
-            padding: "4px 10px",
-            background: "#e0e0e0",
-            border: "1px solid gray",
-            "z-index": 99999,
-            "-moz-border-radius": "8px",
-            "-moz-box-shadow": "2px 2px 6px silver"
-          })
-          .text("Drag to Recycle Bin to delete " + dd.count + " selected row(s)")
-          .appendTo("body");
-
-      dd.helper = proxy;
-
-      $(dd.available).css("background", "pink");
-
-      return proxy;
-    });*/
-    
-    /*
-    //Ondrag Handler
-    this.grid.onDrag.subscribe(function (e, dd) {
-      if (dd.mode != "recycle") {
-        return;
-      }
-      dd.helper.css({top: e.pageY + 5, left: e.pageX + 5});
-    });
-
-    //Drag End Handler
-    this.grid.onDragEnd.subscribe(function (e, dd) {
-      if (dd.mode != "recycle") {
-        return;
-      }
-      dd.helper.remove();
-      $(dd.available).css("background", "beige");
-    });
-    */
-    /*
-    $.drop({mode: "mouse"});
-    $("#dropzone")
-      .bind("dropstart", function (e, dd) {
-        if (dd.mode != "recycle") {
-          return;
-        }
-        $(this).css("background", "yellow");
-      })
-      .bind("dropend", function (e, dd) {
-        if (dd.mode != "recycle") {
-          return;
-        }
-        $(dd.available).css("background", "pink");
-      })
-      .bind("drop", function (e, dd) {
-        if (dd.mode != "recycle") {
-          return;
-        }
-        var rowsToDelete = dd.rows.sort().reverse();
-        for (var i = 0; i < rowsToDelete.length; i++) {
-          data.splice(rowsToDelete[i], 1);
-        }
-        self.grid.invalidate();
-        self.grid.setSelectedRows([]);
-      });
-      */
-      
-
-    /* end row reordering support*/
     self.grid.registerPlugin(moveRowsPlugin);
-    /*
+    
+    /* end row reordering support*/
+    
     this._slickHandler.subscribe(this.grid.onSort, function(e, args){
       var order = (args.sortAsc) ? 'asc':'desc';
       var sort = [{
@@ -475,7 +361,6 @@ my.SlickGrid = Backbone.View.extend({
       }];
       self.model.query({sort: sort});
     });
-
     
     this._slickHandler.subscribe(this.grid.onColumnsReordered, function(e, args){
       self.state.set({columnsOrder: _.pluck(self.grid.getColumns(),'id')});
@@ -514,8 +399,6 @@ my.SlickGrid = Backbone.View.extend({
     }) ;
     var columnpicker = new Slick.Controls.ColumnPicker(columns, this.grid,
                                                        _.extend(options,{state:this.state}));
-
-    */
     if (self.visible){
       self.grid.init();
       self.rendered = true;
@@ -523,8 +406,6 @@ my.SlickGrid = Backbone.View.extend({
       // Defer rendering until the view is visible
       self.rendered = false;
     }
-    
-   
     return this;
 
   },
