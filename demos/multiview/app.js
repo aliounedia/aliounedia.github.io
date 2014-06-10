241b49c6747f8974ae24e4857d6cdb10938dda70
aliounedia <dia.aliounes@gmail.com>
2014-06-10 21:19:21 +0200

    update

--- demos/multiview/app.js
+++ demos/multiview/app.js
@@ -65,8 +65,8 @@ var createExplorer = function(dataset, state) {
     reload = true;
   }
   window.dataExplorer = null;
-  //var $el = $('<div />');
-  //$el.appendTo(window.explorerDiv);
+  var $el = $('<div />');
+  $el.appendTo(window.explorerDiv);
 
   var views = [
     {
@@ -121,6 +121,8 @@ var createExplorer = function(dataset, state) {
     model: dataset,
     el: $el
   })
-  window.dataExplorer.render().el.appendTo(window.explorerDiv)
+  window.dataExplorer.render()
+  alert(dataExplorer.el)
+
 }
 
