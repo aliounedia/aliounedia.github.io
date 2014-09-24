717a4ddab8ae1a5684fe9243fa1aeb5e2f54bc38
aliounedia <dia.aliounes@gmail.com>
2014-09-24 17:22:41 +0200

    update

--- _includes/example-backends-online-csv.js
+++ _includes/example-backends-online-csv.js
@@ -3,15 +3,29 @@
 var dataset = new recline.Model.Dataset({
   url: '{{page.root}}/demos/data/sample.csv',
   backend: 'csv',
-  // delimiter: ',',
-  // quotechar: '"',
-  // encoding: 'utf8'
+  state: {
+          gridOptions: {
+            editable: true,
+	    // Enable support for row add
+            enabledAddRow: true,
+	    // Enable support for row delete
+	    enabledDelRow: true,
+	    // Enable support for row Reoder 
+	    enableReOrderRow:true,
+            autoEdit: false,
+            enableCellNavigation: true
+          },
+          columnsEditor: [
+            { column: 'date', editor: Slick.Editors.Date },
+            { column: 'title', editor: Slick.Editors.Text }
+          ]
+        }
+
 });
 
 // remember this is async so if you want to do something you need to call it in done method e.g.
 // dataset.fetch.done(function(dataset) { console.log(dataset.recordCount)});
-
-// dataset.fetch();
+dataset.fetch();
 
 // show the data for illustrations sake
 var grid = new recline.View.SlickGrid({
@@ -19,11 +33,4 @@ var grid = new recline.View.SlickGrid({
 });
 $('#my-online-csv').append(grid.el);
 
-dataset.fetch().done(function(dataset) {
-  alert("records")
-  alert(dataset.records.toJSON())
-  if (console) {
-    console.log(dataset.records.toJSON());
-  }
-});
 
